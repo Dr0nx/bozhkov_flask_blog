ff3103e73207c0c5d804e33b539cfdc2bf83836c
Andrey Bozhkov <kovbozh@gmail.com>
2022-07-09 00:02:45 +0300

    Update __init__.py

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -1,35 +1,49 @@
 from flask import Flask
+from flask_admin import Admin
+from flask_babel import Babel
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
 
 from flask_blog.config import Config
+from flask_blog.errors.handlers import errors
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 bcrypt = Bcrypt()
 mail = Mail()
+babel = Babel()
 
 
 def create_app(config_class=Config):
-    # print(__name__)
     app = Flask(__name__)
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config.from_object(Config)
 
     db.init_app(app)
     login_manager.init_app(app)
     bcrypt.init_app(app)
     mail.init_app(app)
+    babel.init_app(app)
 
     from flask_blog.main.routes import main
     from flask_blog.users.routes import users
     from flask_blog.posts.routes import posts
+    from flask_blog.admin.routes import MyAdminMainView
+    from flask_blog.admin.views import UserView, PostView, CommentView, TagView, MyAdminModelView
+    from flask_blog.models import User, Post, Comment, Tag
+
 
     app.register_blueprint(main)
     app.register_blueprint(users)
     app.register_blueprint(posts)
-    # app.register_blueprint(errors)
+    app.register_blueprint(errors)
+
+    admin = Admin(app, 'Блог Андрея', index_view=MyAdminMainView(), template_mode='bootstrap4', url='/')
+    admin.add_view(UserView(User, db.session, name='Пользователь'))
+    admin.add_view(PostView(Post, db.session, name='Статьи'))
+    admin.add_view(CommentView(Comment, db.session, name='Комментарии'))
+    admin.add_view(TagView(Tag, db.session, name='Теги'))
+    # admin.add_view(MyAdminModelView(User, db.session, name='Админка'))
 
     return app
