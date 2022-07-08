052aa649aeae7e009893a7c02d73668fb89402f7
Andrey Bozhkov <kovbozh@gmail.com>
2022-07-09 00:02:54 +0300

    Update config.py

--- flask_blog/config.py
+++ flask_blog/config.py
@@ -1,6 +1,13 @@
+import os
+
+
 class Config:
+    FLASK_ENV = 'development'
+    SECRET_KEY = os.urandom(20)
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
-    SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    FLASK_ADMIN_SWATCH = 'spacelab'
+    BABEL_DEFAULT_LOCALE = 'ru'
 
     MAIL_SERVER = 'smtp.yandex.ru'
     MAIL_PORT = 587
