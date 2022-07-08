31ab8e9fab1f0b39858ceb9cff82622a7c9c643c
Andrey Bozhkov <kovbozh@gmail.com>
2022-07-09 00:03:26 +0300

    Update models.py

--- flask_blog/models.py
+++ flask_blog/models.py
@@ -13,11 +13,17 @@ def load_user(user_id):
 
 
 class User(db.Model, UserMixin):
+    __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.png')
+    role = db.Column(db.String(100), nullable=False)
+    username = db.Column(db.String(20), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=False, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
+    last_seen = db.Column(db.DateTime)
+    user_status = db.Column(db.String(40), nullable=True, default='Лучший пользователь проекта')
+    tags = db.relationship('Tag', backref='user_tag', lazy=True, cascade="all, delete-orphan")
     posts = db.relationship('Post', backref='author', lazy=True)
 
     # def __repr__(self):
@@ -37,10 +43,38 @@ class User(db.Model, UserMixin):
         return User.query.get(user_id)
 
 
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+# class AdminUser(db.Model, UserMixin):
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(20))
 
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), unique=False, nullable=False)
+    content = db.Column(db.Text(60), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=True)
+    comments = db.relationship('Comment', backref='article', lazy=True, cascade="all, delete-orphan")
+    tags = db.relationship('Tag', backref='post_tag', lazy=True, cascade="all, delete-orphan")
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.now)
+
+
+class Comment(db.Model):
+    __tablename__ = "comments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=False, nullable=False)
+    body = db.Column(db.Text(200), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), nullable=True)
+
+
+class Tag(db.Model):
+    __tablename__ = "tags"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=False, nullable=False)
+    name = db.Column(db.Text(200), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=True)
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), nullable=True)
